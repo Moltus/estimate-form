218f6d9ab1419e31e194f662a6c06cb9fbc5081a
Moltus <moltus@protonmail.com>
2019-05-16 19:15:46 +0200

    cleaned icon components svg markup inside jsx, css animations for all of them

--- src/components/icons/IphoneFunctional.js
+++ src/components/icons/IphoneFunctional.js
@@ -1,4 +1,5 @@
 import React from "react";
+import "./IphoneFunctional.css";
 
 const IphoneFunctional = () => {
   return (
@@ -10,11 +11,11 @@ const IphoneFunctional = () => {
       <title>iphone-functional</title>
       <g class="power">
         <path
-          class="functional__power"
+          class="iphone-functional__power"
           d="M24.85,13.82a6.64,6.64,0,1,0-6.63,6.64A6.64,6.64,0,0,0,24.85,13.82Zm-6,0a.63.63,0,0,1-1.26,0V10.54a.63.63,0,1,1,1.26,0Zm3.63,0a4.29,4.29,0,1,1-7.31-3,.62.62,0,0,1,.44-.18.66.66,0,0,1,.45.18.64.64,0,0,1,0,.89,3,3,0,1,0,4.28,0,.62.62,0,0,1,0-.89.64.64,0,0,1,.89,0A4.26,4.26,0,0,1,22.5,13.83Z"
         />
         <path
-          class="functional__case"
+          class="iphone-functional__case"
           d="M18.22,21.93a8.11,8.11,0,0,1,0-16.22,8,8,0,0,1,2.77.5V2.54A1.91,1.91,0,0,0,19.08.63h-11A1.92,1.92,0,0,0,6.16,2.54V24.91a1.91,1.91,0,0,0,1.91,1.9h11A1.9,1.9,0,0,0,21,24.91V21.43A8,8,0,0,1,18.22,21.93Z"
         />
       </g>
--- src/components/icons/IphoneScreen.js
+++ src/components/icons/IphoneScreen.js
@@ -1,4 +1,5 @@
 import React from "react";
+import "./IphoneScreen.css";
 
 const IphoneScreen = () => {
   return (
@@ -8,31 +9,24 @@ const IphoneScreen = () => {
       className="question-icon"
     >
       <title>iphone-screen</title>
-      <g id="background">
-        <rect class="cls-1" width="27.44" height="27.44" />
-      </g>
-      <g id="iphone">
+      <g className="iphone-screen">
         <path
-          id="case"
-          class="cls-2"
+          className="iphone-screen__case"
           d="M19.31.45H8.14A1.93,1.93,0,0,0,6.2,2.38V25.06A1.94,1.94,0,0,0,8.14,27H19.31a1.94,1.94,0,0,0,1.93-1.94V2.38A1.93,1.93,0,0,0,19.31.45Zm.48,20.41"
         />
         <rect
-          id="screen"
-          class="cls-3"
+          className="iphone-screen__screen"
           x="7.65"
           y="3.71"
           width="12.14"
           height="17.15"
         />
         <path
-          id="button"
-          class="cls-2"
+          className="iphone-screen__button"
           d="M13.72,25.09A1.24,1.24,0,1,1,15,23.85,1.24,1.24,0,0,1,13.72,25.09Z"
         />
         <path
-          id="speaker"
-          class="cls-2"
+          className="iphone-screen__speaker"
           d="M11,1.62h5.52c.14,0,.25.21.25.47s-.11.46-.25.46H11c-.14,0-.25-.21-.25-.46S10.82,1.62,11,1.62Z"
         />
       </g>
